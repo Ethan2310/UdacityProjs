1d314e186671e6db6d6af3f28756204e5f829d83
Ethan2310 <89082288+Ethan2310@users.noreply.github.com>
2024-03-01 09:44:14 +0200

    Delete UdaSecurityProj directory

--- UdaSecurityProj/Image/src/main/java/service/com/udacity/catpoint/image/service/ImageService.java
+++
@@ -1,8 +0,0 @@
-package service.com.udacity.catpoint.image.service;
-
-import java.awt.image.BufferedImage;
-
-public interface ImageService {
-
-    boolean imageContainsCat(BufferedImage img, float Confidence);
-}
