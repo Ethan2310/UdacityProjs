7a5b94ec054424887235950707a30ec27c311fd8
Ethan2310 <89082288+Ethan2310@users.noreply.github.com>
2024-03-02 23:02:39 +0200

    Delete UdaSecurityProj directory

--- UdaSecurityProj/Image/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module Image {
-    requires org.slf4j;
-    requires software.amazon.awssdk.auth;
-    requires software.amazon.awssdk.core;
-    requires software.amazon.awssdk.regions;
-    requires software.amazon.awssdk.services.rekognition;
-    requires java.desktop;
-    exports service.com.udacity.catpoint.image.service;
-}
